65a72b651f9fd383ef923d05f138d0beab4b97b7
Martian <81148675+Martian-0007@users.noreply.github.com>
2025-12-03 18:39:36 +0100

    Add symlink path parsing, logging, and Synchronizer class attribute type hints
    Signed-off-by: Martian <81148675+Martian-0007@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import logging
+import os
 import sys
 import time
 
@@ -9,7 +10,7 @@ import time
 class Synchronizer:
     """Class for the Folder Synchronizer."""
 
-    def __init__(self, source, replica, interval_secs, count):
+    def __init__(self, source: str, replica: str, interval_secs: int, count: int):
         """Initialize the Synchronizer attributes for later use.
 
         Args:
@@ -18,14 +19,24 @@ class Synchronizer:
             interval_secs: interval between two synchronizations in seconds
             count: count of synchronizations
         """
-        self.source = source
-        self.replica = replica
-        self.interval = interval_secs
-        self.count = count
+        self.source: str = source
+        self.replica: str = replica
+        self.interval: int = interval_secs
+        self.count: int = count
+
+        self.source_abs = os.path.abspath(self.source)
+        self.replica_abs = os.path.abspath(self.replica)
 
     def run(self):
         """Run the synchronizer."""
         logging.info("Folder Syncer Running")
+
+        logging.info(
+            f"Source: {self.source_abs}",
+        )
+        logging.info(f"Replica: {self.replica_abs}")
+        logging.debug(f"Interval: {self.interval} s")
+        logging.debug(f"Count: {self.count}")
 
         for i in range(self.count):
             self._sync()
@@ -34,6 +45,31 @@ class Synchronizer:
 
     def _sync(self):
         pass  # TODO
+
+    def _symlink_in_source(self, symlink_path) -> str:
+        """Check if a symlink is in the source folder.
+
+        Returns absolute path in case symlink points outside of source folder.
+
+        Args:
+            symlink_path: path to symlink
+
+        Returns:
+            str: path of symlink to be written
+        """
+        symlink_path_abs = os.path.abspath(os.path.join(self.source, symlink_path))
+
+        logging.debug(f"Symlink absolute path: {symlink_path_abs}")
+
+        if self.source_abs == os.path.commonpath(
+            [self.source_abs, symlink_path_abs]
+        ):  # alternatively `self.source_abs in symlink_path_abs[:len(self.source_abs)]`
+            return symlink_path
+        else:
+            logging.debug(
+                f"Symlink path leads outside of source folder: {symlink_path_abs}"
+            )
+            return symlink_path_abs
 
 
 def main():
