e8e57fb1caf0554dbc9feb130a4a50430e287f8e
Martian <81148675+Martian-0007@users.noreply.github.com>
2025-12-03 22:18:54 +0100

    Add improved logging, copying logic, and other improvements
    Signed-off-by: Martian <81148675+Martian-0007@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import os
+import shutil
 import sys
 import time
 
@@ -27,49 +28,129 @@ class Synchronizer:
         self.source_abs = os.path.abspath(self.source)
         self.replica_abs = os.path.abspath(self.replica)
 
+        logging.info(f"Source: {self.source}")
+        logging.debug(f"Source absolute: {self.source_abs}")
+
+        logging.info(f"Replica: {self.replica}")
+        logging.debug(f"Replica absolute: {self.replica_abs}")
+
+        logging.debug(f"Interval: {self.interval} s")
+        logging.debug(f"Count: {self.count}")
+
     def run(self):
         """Run the synchronizer."""
         logging.info("Folder Syncer Running")
 
-        logging.info(
-            f"Source: {self.source_abs}",
-        )
-        logging.info(f"Replica: {self.replica_abs}")
-        logging.debug(f"Interval: {self.interval} s")
-        logging.debug(f"Count: {self.count}")
-
         for i in range(self.count):
             self._sync()
             logging.info(f"Folder sync Completed {i + 1} times")
-            time.sleep(self.interval)
+            if i < self.count - 1: # no need to sleep on the last sync
+                time.sleep(self.interval)
 
     def _sync(self):
-        pass  # TODO
+        """Synchronize source folder to replica folder."""
+        if not os.path.exists(self.replica_abs):
+            logging.debug(f"{self.replica_abs} does not exist, creating directory...")
+            os.mkdir(self.replica_abs)
 
-    def _symlink_in_source(self, symlink_path) -> str:
-        """Check if a symlink is in the source folder.
+        if not os.path.isdir(self.replica_abs):
+            logging.error(f"{self.replica_abs} is not a directory! Quitting")
+            quit()
 
-        Returns absolute path in case symlink points outside of source folder.
+        logging.debug("Cleaning replica folder")
+        self._clean(self.replica_abs)
+
+        self._copyfolder(self.source, self.replica)
+
+    def _symlink_path_handler(self, symlink_path, symlink_path_absolute) -> str:
+        """Check if a symlink is pointing inside the source folder.
+
+        Returns absolute path of the original link in case symlink points outside the source folder.
 
         Args:
-            symlink_path: path to symlink
+            symlink_path: path of symlink
+            symlink_path: absolute path of symlink
 
         Returns:
-            str: path of symlink to be written
+            str: path to which the symlink should point
         """
-        symlink_path_abs = os.path.abspath(os.path.join(self.source, symlink_path))
-
-        logging.debug(f"Symlink absolute path: {symlink_path_abs}")
+        logging.debug(f"Symlink absolute path: {symlink_path_absolute}")
 
         if self.source_abs == os.path.commonpath(
-            [self.source_abs, symlink_path_abs]
+            [self.source_abs, symlink_path_absolute]
         ):  # alternatively `self.source_abs in symlink_path_abs[:len(self.source_abs)]`
+            logging.debug(f"Symlink inside source, using {symlink_path}")
             return symlink_path
         else:
             logging.debug(
-                f"Symlink path leads outside of source folder: {symlink_path_abs}"
+                f"Symlink path leads outside of source folder, using absolute path: {symlink_path_absolute}"
             )
-            return symlink_path_abs
+            return symlink_path_absolute
+
+    def _clean(self, folder_path: str):
+        """Clean the folder."""
+        # An alternative idea was to remove the whole replica folder and recreate it.
+        # This could, however, rewrite metadata it shouldn't.
+
+        if os.path.exists(folder_path):
+            for i in os.listdir(folder_path):
+                logging.info(f"Remove: {os.path.abspath(os.path.join(folder_path, i))}")
+
+                if os.path.isdir(os.path.join(folder_path, i)):
+                    shutil.rmtree(os.path.join(folder_path, i)) # remove even non-empty
+                else:
+                    os.remove(os.path.join(folder_path, i))
+
+    def _copyfolder(self, src, dst):
+        """Copy source folder to destination.
+
+        Args:
+            src: path to source folder
+            dst: path to destination folder
+        """
+        contents = os.scandir(src)
+
+        logging.info(f"Copy: {os.path.abspath(src)} to {os.path.abspath(dst)}")
+
+        if not os.path.exists(dst):
+            os.mkdir(dst)
+
+        for i in contents:
+            if i.is_dir(follow_symlinks=False):
+                self._copyfolder(os.path.join(src, i.name), os.path.join(dst, i.name))
+
+            elif i.is_junction():
+                logging.warn(
+                    f"Junction in path {os.path.realpath(os.path.join(src, i.name))}"
+                )
+
+                logging.info(
+                    f"Copy: {os.path.abspath(i.path)} to {os.path.abspath(os.path.join(dst, i.name))}"
+                )
+
+                shutil.copy2(i.path, os.path.join(dst, i.name))
+
+            elif i.is_symlink():
+                source_link_path = os.readlink(i.path)
+
+                logging.info(
+                    f"Copy: {os.path.abspath(i.path)} to {os.path.abspath(os.path.join(dst, i.name))}"
+                )
+
+                os.symlink(
+                    self._symlink_path_handler(source_link_path, os.path.abspath(os.path.join(src, source_link_path))),
+                    os.path.join(dst, i.name),
+                )
+
+            elif i.is_file():
+                logging.info(
+                    f"Copy: {os.path.abspath(i.path)} to {os.path.abspath(os.path.join(dst, i.name))}"
+                )
+
+                shutil.copy2(i.path, os.path.join(dst, i.name))
+
+            else:
+                logging.error(f"Unknown file type: {os.path.abspath(i.path)}")
 
 
 def main():
@@ -88,12 +169,13 @@ def main():
 
     args = parser.parse_args()
 
-    logging.getLogger(__name__).addHandler(logging.StreamHandler(sys.stdout))
+    logging.getLogger(__name__).addHandler(logging.StreamHandler())
     logging.basicConfig(
         level=logging.DEBUG,
-        filename=args.logfile,
+        #filename=args.logfile,
+        #filemode="w"
         format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-        filemode="w",
+        stream=sys.stderr
     )
 
     syncer = Synchronizer(args.source, args.replica, args.interval_seconds, args.count)
